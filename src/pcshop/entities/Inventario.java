ea3a7b355e2d62bbf572d15f93c912f131ce6986
LeoOlivaresD <cspraword@gmail.com>
2024-09-02 23:52:35 -0400

    Termino de actividad

--- src/pcshop/entities/Inventario.java
+++ src/pcshop/entities/Inventario.java
@@ -1,5 +1,164 @@
 package pcshop.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Inventario {
-    
+
+    private List<Producto> listaProductos;
+
+    public Inventario() {
+        this.listaProductos = new ArrayList<>();
+    }
+
+    public Inventario(List<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    //Metodo para generar ids automaticos en productos
+    public Integer generarIdProducto() {
+        Random randomNumero = new Random();
+        int idGenerado = randomNumero.nextInt(1000) + 1;
+        return idGenerado;
+    }
+
+    //Obtengo un producto por medio de su ID
+    public Producto obtenerProductoPorId(int idProd) {
+        Producto productoBuscado = new Producto();
+        for (Producto productoEncontrado : listaProductos) {
+            if (productoEncontrado.getIdProducto() == idProd) {
+                System.out.println("Producto encontrado !");
+                productoBuscado = productoEncontrado;
+                return productoBuscado;
+            } else {
+                System.out.println("Lo sentimos, producto no encotrado");
+            }
+        }
+        return null;
+    }
+
+    public Producto obtenerProductoPorNombre(String nombreProd) {
+        Producto productoBuscado = new Producto();
+        for (Producto productoEncontrado : listaProductos) {
+            if (productoEncontrado.getNombreProducto().equalsIgnoreCase(nombreProd)) {
+                System.out.println("Producto encontrado !");
+                productoBuscado = productoEncontrado;
+                return productoBuscado;
+            } else {
+                System.out.println("Lo sentimos, no encontramos el producto");
+            }
+        }
+        return null;
+    }
+
+    public void agregarProducto(Producto producto) {
+
+        Scanner sc = new Scanner(System.in);
+        producto.setIdProducto(generarIdProducto());
+        System.out.println("Id generado: " + producto.getIdProducto());
+        System.out.println("Ingrese nombre para producto: ");
+        String nombreProducto = sc.nextLine();
+        producto.setNombreProducto(nombreProducto);
+        System.out.println("Nombre agregado con exito !");
+        System.out.println("Ingrese una descripcion para el producto: ");
+        String descripcionProd = sc.nextLine();
+        producto.setDescripcionProducto(descripcionProd);
+        System.out.println("Descripcion agregada con exito !");
+        System.out.println("Ingrese precio del producto: ");
+        double precio = sc.nextDouble();
+        producto.setPrecioProducto(precio);
+        System.out.println("Precio agregado con exito !");
+        System.out.println("Ingrese cantidad de stock para productos");
+        int stock = sc.nextInt();
+        producto.setCantidadStockProducto(stock);
+        System.out.println("Stock agregado con exito !");
+        listaProductos.add(producto);
+
+        if (!listaProductos.isEmpty()) {
+            System.out.println("Producto agregado con exito al inventario");
+        }
+
+    }
+
+    public void actualizarProducto(int idProd) {
+        Scanner sc = new Scanner(System.in);
+        Producto productoBuscado;
+        for (Producto productoEncontrado : listaProductos) {
+            if (productoEncontrado.getIdProducto() == idProd) {
+                productoBuscado = productoEncontrado;
+                System.out.println("Ingrese nombre para producto: ");
+                String nombreProducto = sc.nextLine();
+                productoBuscado.setNombreProducto(nombreProducto);
+                System.out.println("Nombre agregado con exito !");
+                System.out.println("Ingrese una descripcion para el producto: ");
+                String descripcionProd = sc.nextLine();
+                productoBuscado.setDescripcionProducto(descripcionProd);
+                System.out.println("Descripcion agregada con exito !");
+                System.out.println("Ingrese precio del producto: ");
+                double precio = sc.nextDouble();
+                sc.nextLine();
+                productoBuscado.setPrecioProducto(precio);
+                System.out.println("Precio agregado con exito !");
+                System.out.println("Ingrese cantidad de stock para productos");
+                int stock = sc.nextInt();
+                sc.nextLine();
+                productoBuscado.setCantidadStockProducto(stock);
+                System.out.println("Stock agregado con exito !");
+                //listaProductos.add(productoEncontrado); se pongo esta linea mi producto quedaria duplicado con el id
+                System.out.println("Producto actualizado con exito");
+            }
+        }
+
+    }
+
+    //Elimina un producto por el id
+    public void eliminarProductoPorId(int idProd) {
+        Producto productoAEliminar = null;
+        for (Producto producto : listaProductos) {
+            if (producto.getIdProducto() == idProd) {
+                productoAEliminar = producto;
+                break;
+            }
+        }
+        if (productoAEliminar != null) {
+            listaProductos.remove(productoAEliminar);
+            System.out.println("Producto eliminado con éxito!");
+        } else {
+            System.out.println("Producto no encontrado.");
+        }
+    }
+
+    //Listar productos
+    public void listarTodosLosProductos() {
+        System.out.println("Listando productos");
+        for (Producto p : listaProductos) {
+            System.out.println(p);
+        }
+    }
+
+    //Buscar producto por descripcion
+    public void buscarProdPorDescripcion(String descripcion) {
+        Producto productoBuscado = null;
+        for (Producto productoEncontrado : listaProductos) {
+            if (productoEncontrado.getDescripcionProducto().contains(descripcion)) {
+                productoBuscado = productoEncontrado;
+                System.out.println(productoBuscado);
+            }else{
+            productoBuscado = null;
+            }
+        }
+        if(productoBuscado == null){
+            System.out.println("Lo sentimos, no hay productos con esa descripcion");
+        }
+    }
 }
--- src/pcshop/entities/MenuPrincipal.java
+++ src/pcshop/entities/MenuPrincipal.java
@@ -1,6 +1,79 @@
 package pcshop.entities;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class MenuPrincipal {
-    
+
+    public void ejecutarMenuPrincipal(Producto producto, Inventario inventario) {
+        Scanner sc = new Scanner(System.in);
+        int opcionMenu = 0;
+        boolean salirCiclo = false;
+        do{
+        try{
+        System.out.println("Digite el numero de la opcion que desea ingresar: ");
+        System.out.println("1: Agregar producto.\n"
+                + "2: Buscar producto por nombre.\n"
+                + "3: Actualizar producto por ID.\n"
+                + "4: Eliminar producto por Id.\n"
+                + "5: Listar todos los productos.\n"
+                + "6: Buscar producto por descripcion.\n"
+                + "7: Salir.");
+        
+        opcionMenu = sc.nextInt();
+        sc.nextLine();
+        }catch(InputMismatchException e){
+            System.out.println("Dato ingresado incorrecto,por favor elegi un numero valido");
+            sc.nextLine(); //Limpio el buffer de mi scanner
+            continue;       //reinicio el ciclo
+        }
+        
+        switch (opcionMenu) {
+            //AGREGAR PRODUCTO A INVENTARIO
+            case 1:
+                inventario.agregarProducto(producto);
+                break;
+                //BUSCAR PRODUCTO POR NOMBRE
+            case 2:
+                System.out.println("Ingrese el nombre del prducto que desea buscar:");
+                String nombreProd = sc.nextLine();
+                inventario.obtenerProductoPorNombre(nombreProd);
+                break;
+                //ACTUALIZAR PRODUCTO POR ID
+            case 3:
+                System.out.println("Mostrando productos..");
+                inventario.listarTodosLosProductos();
+                System.out.println("Ingrese el id del producto que desea actualizar");
+                int idProd = sc.nextInt();
+                inventario.actualizarProducto(idProd);
+                break;
+                //ELIMINAR PRODUCTO POR ID
+            case 4:
+                System.out.println("Mostrando productos..");
+                inventario.listarTodosLosProductos();
+                System.out.println("Ingrese el id del producto que desea eliminar");
+                idProd = sc.nextInt();
+                inventario.eliminarProductoPorId(idProd);
+                break;
+                //LISTAR TODOS LOS PRODUCTOS
+            case 5:
+                inventario.listarTodosLosProductos();
+                break;
+                //BUSCAR PRODUCTO POR DESCRIPCION
+            case 6:
+                System.out.println("Ingrese la una palabra que este dentro de la descripcion de un producto para poder localizarlo");
+                String descripcion = sc.nextLine();
+                inventario.buscarProdPorDescripcion(descripcion);
+                break;
+                //SALIR
+            case 7:
+                System.out.println("Saliendo del sistema..");
+                salirCiclo = true;
+                break;
+                
+            default:
+                    System.out.println("Lo sentimos la opcion ingresada no es valida");
+        }
+        }while(!salirCiclo);
+    }
 }
