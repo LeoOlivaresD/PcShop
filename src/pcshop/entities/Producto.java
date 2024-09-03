4f92056c286c741e77ec6fc585220f932d43b98e
LeoOlivaresD <cspraword@gmail.com>
2024-09-02 21:00:50 -0400

    Termino Clase producto

--- src/pcshop/entities/Producto.java
+++ src/pcshop/entities/Producto.java
@@ -3,5 +3,71 @@ package pcshop.entities;
 
 
 public class Producto {
+    Integer idProducto;
+    String nombreProducto;
+    String descripcionProducto;
+    Double precioProducto;
+    Integer cantidadStockProducto;
+
+    //Contructores
+    public Producto() {
+    }
+    
+
+    public Producto(Integer idProducto, String nombreProducto, String descripcionProducto, Double precioProducto, Integer cantidadStockProducto) {
+        this.idProducto = idProducto;
+        this.nombreProducto = nombreProducto;
+        this.descripcionProducto = descripcionProducto;
+        this.precioProducto = precioProducto;
+        this.cantidadStockProducto = cantidadStockProducto;
+    }
+    
+    //Getters & Setters
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getDescripcionProducto() {
+        return descripcionProducto;
+    }
+
+    public void setDescripcionProducto(String descripcionProducto) {
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    public Double getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(Double precioProducto) {
+        this.precioProducto = precioProducto;
+    }
+
+    public Integer getCantidadStockProducto() {
+        return cantidadStockProducto;
+    }
+
+    public void setCantidadStockProducto(Integer cantidadStockProducto) {
+        this.cantidadStockProducto = cantidadStockProducto;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombreProducto=" + nombreProducto + ", descripcionProducto=" + descripcionProducto + ", precioProducto=" + precioProducto + ", cantidadStockProducto=" + cantidadStockProducto + '}';
+    }
+    
     
 }
